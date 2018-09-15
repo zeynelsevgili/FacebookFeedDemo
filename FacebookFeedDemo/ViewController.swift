4966154ade7539d617667f00006d0bb3bd8c7ec4
smart <zeynel.sevgili@gmail.com>
2018-09-15 12:46:58 +0300

    Initial Commit

---
+++ FacebookFeedDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FacebookFeedDemo
+//
+//  Created by Demo on 15.09.2018.
+//  Copyright © 2018 Demo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
